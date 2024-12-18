2f0a1e73291dc41ac6694e5a1945b54c9b887e30
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2024-12-18 17:57:35 +0500

    Drawer implemented and sidebar.tsx imported

--- Frontend/accounting-ledger/src/pages/Dashboard/CreateAccount.tsx
+++ Frontend/accounting-ledger/src/pages/Dashboard/CreateAccount.tsx
@@ -1,11 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
 import { useNavigate } from "react-router-dom"
-import {Button} from "../../components/ui/Button"
-import {CircleDollarSign} from "lucide-react"
+import { Button } from "../../components/ui/Button"
+import { CircleDollarSign } from "lucide-react"
 import axios from "axios"
-import {zodResolver} from "@hookform/resolvers/zod"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-import {z} from "zod"
+import { z } from "zod"
 import {
   Form,
   FormControl,
@@ -34,103 +34,113 @@ import {
   DrawerFooter,
   DrawerHeader,
   DrawerTitle,
-  DrawerTrigger,
 } from "@/components/ui/Drawer"
 
-
 const formSchema = z.object({
-  name: z.string().min(4, {
+  name: z
+    .string()
+    .min(4, {
       message: "Name must be at least 4 characters.",
-    }).max(4, {
+    })
+    .max(4, {
       message: "Name must be at most 4 characters.",
-    }).default("cash"),
-    balance: z.coerce.number().min(1, {
-      message: "Balance should be equals to or more than 1.",
-    }),
-    description: z.string().min(10, {
-      message: "Description must be at least 10 characters.",
-    }),
-    status: z.string().min(6, {
+    })
+    .default("cash"),
+  balance: z.coerce.number().min(1, {
+    message: "Balance should be equals to or more than 1.",
+  }),
+  description: z.string().min(10, {
+    message: "Description must be at least 10 characters.",
+  }),
+  status: z
+    .string()
+    .min(6, {
       message: "Status must be at least 6 characters.",
-    }).max(8, {
-      message: "Status must be at most 8 characters."
+    })
+    .max(8, {
+      message: "Status must be at most 8 characters.",
     }),
-    token: z.string().optional()
+  token: z.string().optional(),
 })
 
-
 const CreateAccount = () => {
-    const { toast } = useToast()
+  const { toast } = useToast()
 
-    const form = useForm<z.infer<typeof formSchema>>({
-          resolver: zodResolver(formSchema),
-          defaultValues: {
-              name: "cash",
-              balance: 0,
-              description: "",
-              status: "active"
-          }
+  // State for Drawer visibility and form values
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+  const [submittedValues, setSubmittedValues] = useState<z.infer<typeof formSchema> | null>(null)
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "cash",
+      balance: 0,
+      description: "",
+      status: "active",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    const token = localStorage.getItem("token")
+
+    if (!token) {
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: "You are not logged in",
       })
+      return
+    }
 
-      function onSubmit(values: z.infer<typeof formSchema>) {
-            // I will post the values to backend api at `/api/v2/easy-accounts`
-            // then If I get a success response I will open drawer automatically
-            console.log(values)
+    // Add token to values
+    values.token = token
 
-            const token = localStorage.getItem("token")
+    try {
+      axios
+        .post("/api/v2/easy-accounts", values)
+        .then((response) => {
+          console.log(response.data)
+          toast({
+            variant: "success",
+            title: "Success",
+            description: "Account created successfully",
+          })
 
-            if (!token) {
-              toast({
-                variant: "destructive",
-                title: "Error",
-                description: "You are not logged in",
-              })
-              return
-            }
+          // Store submitted values and open the drawer
+          setSubmittedValues(values)
+          setIsDrawerOpen(true)
 
-            // push the token to values
-            values.token = token
-
-            try {
-              axios.post("/api/v2/easy-accounts", values)
-              .then((response) => {
-                console.log(response.data);
-                toast({
-                  variant: "success",
-                  title: "Success",
-                  description: "Account created successfully",
-                })
-                form.reset()
-                form.setValue("name", "cash")
-                form.setValue("balance", 0)
-                form.setValue("description", "")
-                form.setValue("status", "active")
-               
-              })
-              .catch((error) => {
-                console.log(error);
-                toast({
-                  variant: "destructive",
-                  title: "Error",
-                  description: `Error while creating account: ${error.response?.data.message}`, 
-                })
-              });
-            } catch (error) {
-              console.log(error)
-              toast({
-                variant: "destructive",
-                title: "Error",
-                description: "Error while creating account",
-              })
-            }
-
-      }
+          // Reset the form
+          form.reset()
+          form.setValue("name", "cash")
+          form.setValue("balance", 0)
+          form.setValue("description", "")
+          form.setValue("status", "active")
+        })
+        .catch((error) => {
+          console.log(error)
+          toast({
+            variant: "destructive",
+            title: "Error",
+            description: `Error while creating account: ${error.response?.data.message}`,
+          })
+        })
+    } catch (error) {
+      console.log(error)
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: "Error while creating account",
+      })
+    }
+  }
 
   return (
-    <> <div className="container mx-auto flex items-center justify-center h-screen">
-            <Form {...form}>
+    <>
+      <div className="container mx-auto flex items-center justify-center h-screen">
+        <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
+            <FormField
               control={form.control}
               name="name"
               render={({ field }) => (
@@ -151,8 +161,8 @@ const CreateAccount = () => {
                 </FormItem>
               )}
             />
-    
-    <FormField
+
+            <FormField
               control={form.control}
               name="balance"
               render={({ field }) => (
@@ -165,8 +175,8 @@ const CreateAccount = () => {
                 </FormItem>
               )}
             />
-    
-    <FormField
+
+            <FormField
               control={form.control}
               name="description"
               render={({ field }) => (
@@ -179,42 +189,63 @@ const CreateAccount = () => {
                 </FormItem>
               )}
             />
-    
-    
-    <FormField
+
+            <FormField
               control={form.control}
               name="status"
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Status</FormLabel>
-                  <Select onValueChange={field.onChange} >
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select a status" />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                    <SelectItem value="active">ACTIVE</SelectItem>
-                    <SelectItem value="inactive">INACTIVE</SelectItem>
-                  </SelectContent>
+                  <Select onValueChange={field.onChange}>
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a status" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="active">ACTIVE</SelectItem>
+                      <SelectItem value="inactive">INACTIVE</SelectItem>
+                    </SelectContent>
                   </Select>
                   <FormMessage />
                 </FormItem>
               )}
             />
-    
-  
-            <Button className="w-full" type="submit"><CircleDollarSign />Submit</Button>
+
+            <Button className="w-full" type="submit">
+              <CircleDollarSign />
+              Submit
+            </Button>
           </form>
         </Form>
+      </div>
 
+      {/* Drawer to display form values */}
+      <Drawer open={isDrawerOpen} onOpenChange={setIsDrawerOpen}>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle className="text-center">Account Created</DrawerTitle>
+            <DrawerClose />
+          </DrawerHeader>
+          {/* align to center */}
+          <DrawerDescription className="space-y-2 text-center">
+            {submittedValues && (
+              <div className="space-y-2 text-center">
+                <p className="font-bold text-lg"><strong>Name:</strong> {submittedValues.name}</p>
+                <p className="font-bold text-lg"><strong>Balance:</strong> {submittedValues.balance}</p>
+                <p className="font-bold text-lg"><strong>Description:</strong> {submittedValues.description}</p>
+                <p className="font-bold text-lg"><strong>Status:</strong> <span className={submittedValues.status === "active" ? "text-green-500" : "text-red-500"}>{submittedValues.status}</span></p>
+              </div>
+            )}
+          </DrawerDescription>
+          <DrawerFooter>
+            <Button onClick={() => setIsDrawerOpen(false)}>Close</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
 
-
-            </div>
-
-              
-    
-              <Toaster /></>
+      <Toaster />
+    </>
   )
 }
 
