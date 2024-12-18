2d15fba2be06ee5ff2b953dc1450568c5f36e390
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2024-12-18 20:23:52 +0500

    Updated

--- Frontend/accounting-ledger/src/App.jsx
+++ Frontend/accounting-ledger/src/App.jsx
@@ -1,10 +1,19 @@
 import { Outlet } from 'react-router-dom'
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/Sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
 
 function App() {
 
   return (
     <>
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        
    <Outlet/>
+      </main>
+    </SidebarProvider>
     </>
   )
 }
